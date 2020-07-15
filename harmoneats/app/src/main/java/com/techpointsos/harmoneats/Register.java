cd3e4d4d3d989b33ccfaf1a0f0345551d1f9bedb
Venkatanarayanan, Sathyan <sathyan.1997@gmail.com>
2020-07-15 19:35:39 -0400

    Added User Profile creation and Email Verification

--- harmoneats/app/src/main/java/com/techpointsos/harmoneats/Register.java
+++ harmoneats/app/src/main/java/com/techpointsos/harmoneats/Register.java
@@ -1,11 +1,13 @@
 package com.techpointsos.harmoneats;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,9 +15,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Register extends AppCompatActivity {
 
@@ -23,9 +34,10 @@ public class Register extends AppCompatActivity {
     private Button regButton;
     private TextView mLoginButton;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore fstore;
     private VerifyEmailActivity mVerify;
-    private String phoneNumber;
-
+    private String phoneNumber, userID, name, email, phone;
+    private static final String TAG = "RegisterActivity";
 
 
     @Override
@@ -41,6 +53,7 @@ public class Register extends AppCompatActivity {
         regButton = findViewById((R.id.register));
         mLoginButton = findViewById(R.id.glogin);
         mAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
 
         if(mAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -59,11 +72,11 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String name = uname.getText().toString().trim();
-                String email = uemail.getText().toString().trim();
+                name = uname.getText().toString().trim();
+                email = uemail.getText().toString().trim();
                 String pass1 = upass1.getText().toString().trim();
                 String pass2  = upass2.getText().toString().trim();
-
+                phone = uphone.getText().toString().trim();
                 if(TextUtils.isEmpty(name)){
                     uname.setError("Username is required");
                     return;
@@ -84,11 +97,40 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
+                if(!pass1.equals(pass2)){
+                    Toast.makeText(Register.this, "Passwords need to match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(phone)){
+                    uphone.setError("Phone number is required");
+                    return;
+                }
+
                 mAuth.createUserWithEmailAndPassword(email, pass1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = mAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fstore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("uname", name);
+                            user.put("email", email);
+                            user.put("phone", phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "Success: User Profile created for "+name+" "+userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "Failure: "+ e.toString());
+                                }
+                            });
+                            Intent verifyIntent = new Intent(getApplicationContext(), VerifyEmailActivity.class);
+                            startActivity(verifyIntent);
                         }
                         else {
                             Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -96,8 +138,7 @@ public class Register extends AppCompatActivity {
                     }
                 });
 
-                Intent verifyIntent = new Intent(getApplicationContext(), VerifyEmailActivity.class);
-                startActivity(verifyIntent);
+
             }
         });
     }
--- harmoneats/app/src/main/java/com/techpointsos/harmoneats/VerifyEmailActivity.java
+++ harmoneats/app/src/main/java/com/techpointsos/harmoneats/VerifyEmailActivity.java
@@ -2,6 +2,8 @@ package com.techpointsos.harmoneats;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +14,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,49 +25,88 @@ public class VerifyEmailActivity extends AppCompatActivity {
     private Button sendVerificationButton, checkVerifyButton;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
+    private static final String TAG = "VerifyEmailActivity";
+    private Handler handler;
+    private Runnable runnableCode;
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_verification);
-
+        handler = new Handler();
         mAuth = FirebaseAuth.getInstance();
         checkVerifyButton = findViewById(R.id.checkVerifiedButton);
         sendVerificationButton = findViewById(R.id.sendVerificationButton);
 
-        user = mAuth.getCurrentUser();      //Get necessary current user information.
+        user = mAuth.getCurrentUser();   //Get necessary current user information.
 
-        setCheckVerifyButton(checkVerifyButton);
-        setSendVerifyButton(sendVerificationButton);
+        sendVerificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user != null){   //Avoid Null Pionter Exceptions
+                    user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(VerifyEmailActivity.this, "Verification email sent", Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Failure: "+ e.toString());
+                        }
+                    });   //Send the user a new verification email.
+                }
+            }
+        });
+        runnableCode = new Runnable() {
+            @Override
+            public void run() {
+                // Do something here on the main thread
+                Log.d(TAG, "Waiting for Verification");
+                user.reload();
+                if(user.isEmailVerified()){
+                    Log.d(TAG, "Success: Email verified");
+                    handler.removeCallbacks(runnableCode);
+                    Toast.makeText(VerifyEmailActivity.this, "User is verified.", Toast.LENGTH_LONG).show();
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    return;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        };
+        //handler.post(runnableCode);
+        runnableCode.run();
+
+
+        //setCheckVerifyButton(checkVerifyButton);
+        //setSendVerifyButton(sendVerificationButton);
+        checkVerifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user != null) {  //Avoid Null Pointer Exceptions
+                    user.reload();
+                    if (user.isEmailVerified()) {   //Check if the email address is verified.
+                        Toast.makeText(VerifyEmailActivity.this, "User is verified.", Toast.LENGTH_LONG).show();
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    } else {    //Instruct user to send a new verification email if they are not verified.
+                        Toast.makeText(VerifyEmailActivity.this, "User is not verified.", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
     /*
         Set the "Send Verification" button to send a new verification email to the user.
      */
     private void setSendVerifyButton(Button sendVerificationButton){
-        sendVerificationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(user != null){   //Avoid Null Pionter Exceptions
-                    user.sendEmailVerification();   //Send the user a new verification email.
-                }
-            }
-        });
+
     }
     /*
         Set the "Check Verification" button to let the user know if they are verified or not.
      */
     private void setCheckVerifyButton(Button checkVerifyButton){
-        checkVerifyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(user != null) {  //Avoid Null Pointer Exceptions
-                    if (user.isEmailVerified()) {   //Check if the email address is verified.
-                        Toast.makeText(VerifyEmailActivity.this, "User is verified.", Toast.LENGTH_LONG).show();
-                    } else {    //Instruct user to send a new verification email if they are not verified.
-                        Toast.makeText(VerifyEmailActivity.this, "Click Send Verification to verify email.", Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-        });
+
     }
 }

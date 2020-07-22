11876f149749f0fef79b2be0b3837d5601fee9e0
meyerc1 <33985500+meyerc1@users.noreply.github.com>
2020-07-22 19:35:33 -0400

    Added a stock avatar photo to the account fragment.

--- harmoneats/app/src/main/java/com/techpointsos/harmoneats/Account.java
+++ harmoneats/app/src/main/java/com/techpointsos/harmoneats/Account.java
@@ -1,18 +1,27 @@
 package com.techpointsos.harmoneats;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,6 +29,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class Account extends Fragment implements RecyclerViewClickInterface{
 
     private TextView nameAccountTextView, emailAccountTextView, passwordReset;
+    private ImageView accountAvatar;
     private FirebaseUser user;
     private FirebaseAuth fAuth;
     private View myView;
@@ -46,10 +56,12 @@ public class Account extends Fragment implements RecyclerViewClickInterface{
         nameAccountTextView = view.findViewById(R.id.nameAccountTextView);
         emailAccountTextView = view.findViewById(R.id.emailAccountTextView);
         passwordReset = view.findViewById(R.id.accountPasswordResetTextView);
+        accountAvatar = (ImageView) view.findViewById(R.id.accountImageView);
 
         if(user != null) {      //If we can't get a user, we need to have them login so we can access user info
-            nameAccountTextView.setText(user.getDisplayName());
+            nameAccountTextView.setText(user.getPhoneNumber());
             emailAccountTextView.setText(user.getEmail());
+            accountAvatar.setImageResource(R.drawable.stock_avatar);
         }
         else{
             Toast.makeText(myView.getContext(), "Sorry, you need to log in first.", Toast.LENGTH_LONG).show();
